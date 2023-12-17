7d5d0835990217ef605be7605a69465d8732ba96
yelaman <67456840+zhanibekov@users.noreply.github.com>
2023-12-17 19:27:10 +0600

    Fixed an error in the state request status and the articles themselvesуу✅

--- src/redux/slices/Posts.js
+++ src/redux/slices/Posts.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async() => {
-    const { data } = await axios.get('http://localhost:3000');
+    const { data } = await axios.get('http://localhost:4444/posts');
     return data;
 })
 
@@ -19,7 +19,23 @@ const initialState = {
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers: {
+        [fetchPosts.pending]: (state) => {
+            state.posts.items = [];
+            state.posts.status = 'loading';
+        },
+        [fetchPosts.fulfilled]: (state, action) => {
+            state.posts.items = action.payload;
+            state.posts.status = 'loaded';
 
+        },
+
+        [fetchPosts.rejected]: (state) => {
+            state.posts.items = [];
+            state.posts.status = 'error';
+
+        }
+    }
 });
 export const postsReducer = postsSlice.reducer;
