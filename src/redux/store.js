f4870ed6be717a955da656fa3361fa8310b908db
yelaman <67456840+zhanibekov@users.noreply.github.com>
2024-04-14 00:33:26 +0500

    m

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { postsReducer } from './slices/Posts';
+import { authReducer } from './slices/Auth';
 
 const store = configureStore({
     reducer: {
         posts: postsReducer,
+        auth: authReducer,
     }
 });
 export default store;
