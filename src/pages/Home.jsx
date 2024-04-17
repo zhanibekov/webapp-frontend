abd428d2f9af43b64cfe33abdbce2bb4ecbbb005
yelaman <67456840+zhanibekov@users.noreply.github.com>
2024-04-17 21:11:10 +0500

    Hello

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -59,7 +59,7 @@ export const Home = () => {
               },
               {
                 user: {
-                  fullName: 'Иван Иванов',
+                  fullName: 'Баукен Оспан',
                   avatarUrl: 'https://mui.com/static/images/avatar/2.jpg',
                 },
                 text: 'When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top',
